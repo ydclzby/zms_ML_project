af372e8d38589e4b390c6140ff80c73822411d5b
QDZHOU <qz1021@ic.ac.uk>
2023-11-06 23:24:11 +0800

    feat:add new plots

--- src/preprocessing/preprocessing.py
+++ src/preprocessing/preprocessing.py
@@ -7,6 +7,7 @@ def get_original_data(stock_name,start_date,end_date):
     #data = data.set_index(pd.Index(range(1,len(data)+1)))
     #data['Date'] = pd.to_datetime(data['Date'])
     data["Difference"] = data["Open"] - data["Close"] #difference between Open and Close
+    data["Price percentage change"] = data["Difference"] / data["Open"] * 100
     data["Volume_change"] = data["Volume"].diff() #difference between Volumes
     return data
 
@@ -25,8 +26,8 @@ def export_data(stock_name,start_date,end_date,window_size):
 
     data_ma = calculate_moving_average(data, window_size)
     
-    file_path_before_rolling = "../data/" + stock_name + "_" + start_date[:4] + "_before.csv"   
-    file_path_after_rolling = "../data/" + stock_name + "_" + start_date[:4] + "_after.csv"   
+    file_path_before_rolling = "../../data/" + stock_name + "_" + start_date[:4] + "_before.csv"   
+    file_path_after_rolling = "../../data/" + stock_name + "_" + start_date[:4] + "_after.csv"   
     data.to_csv(file_path_before_rolling,index = True)
     data_ma.to_csv(file_path_after_rolling,index = True)
 
@@ -43,24 +44,36 @@ def export_data(stock_name,start_date,end_date,window_size):
     plot(data,data_ma)
 
 def plot(data,data_ma):
-    fig, (ax1, ax2) = plt.subplots(1,2,figsize = (20,8))
+    fig, axes = plt.subplots(2,2,figsize = (20,20))
     #plot Close value for both data
-    ax1.set_xlabel("Date")
-    ax1.set_ylabel("Stock Price")
-    ax1.set_title("Close")
-    ax1.plot(data.index, data.loc[:,"Close"], color = "red", label = "raw")
-    ax1.plot(data_ma.index, data_ma.loc[:,"Close"], color = "blue", label = "moving_avg")
-    ax1.legend(loc="upper left")
+    axes[0][0].set_xlabel("Date")
+    axes[0][0].set_ylabel("Stock Price")
+    axes[0][0].set_title("Close")
+    axes[0][0].plot(data.index, data.loc[:,"Close"], color = "red", label = "raw")
+    axes[0][0].plot(data_ma.index, data_ma.loc[:,"Close"], color = "blue", label = "moving_avg")
+    axes[0][0].legend(loc="upper left")
 
     #plot Volume
-    ax2.set_xlabel("Date")
-    ax2.set_ylabel("Volume")
-    ax2.set_title("Volume")
-    ax2.plot(data.index, data.loc[:,"Volume"], color = "black", label = "raw")
-    ax2.legend(loc="upper left")
+    axes[0][1].set_xlabel("Date")
+    axes[0][1].set_ylabel("Volume Change")
+    axes[0][1].set_title("Volume Change")
+    axes[0][1].plot(data.index, data.loc[:,"Volume_change"], color = "black", label = "raw")
+    axes[0][1].legend(loc="upper left")
 
+    #plot price percentage change
+    axes[1][0].set_xlabel("Volume Difference")
+    axes[1][0].set_ylabel("Price Percent Change")
+    axes[1][0].set_title("Price Percent Change")
+    axes[1][0].scatter(data.loc[:,"Volume_change"], data.loc[:,"Price percentage change"], marker = "o", color = "black", label = "raw")
+
+    #plot price
+    axes[1][1].set_xlabel("Volume Difference")
+    axes[1][1].set_ylabel("Price Change")
+    axes[1][1].set_title("Price Change")
+    axes[1][1].scatter(data.loc[:,"Volume_change"]**2, data.loc[:,"Difference"]**2, marker = "o", color = "r", label = "raw")
+    
     plt.show()
-    plt.savefig('../data/plot.png')
+    plt.savefig('../../data/plot.png')
 
 
 
